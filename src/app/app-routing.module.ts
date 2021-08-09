beef1fbcb06ad49a11a7759815c3d0355c44bc5a
chegejohn159 <chegejohn159@gmail.com>
2021-08-10 00:59:19 +0300

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from 'src/app/components/home/home.component'
+import { AboutComponent } from 'src/app/components/about/about.component'
+import { ProgramsComponent } from 'src/app/components/programs/programs.component'
+import { OpportunitiesComponent } from 'src/app/components/opportunities/opportunities.component'
+import { ProjectsComponent } from 'src/app/components/projects/projects.component'
+import { TeamComponent } from 'src/app/components/team/team.component'
+import { ContactComponent } from 'src/app/components/contact/contact.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+
+   { path: '', component: HomeComponent },
+   { path: 'about', component: AboutComponent },
+   { path: 'programs', component: ProgramsComponent },
+   { path: 'projects', component: ProjectsComponent },
+   { path: 'opportunities', component: OpportunitiesComponent },
+   { path: 'team', component: TeamComponent },
+   { path: 'contact', component: ContactComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ProgramsComponent } from './components/programs/programs.component';
+import { OpportunitiesComponent } from './components/opportunities/opportunities.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { TeamComponent } from './components/team/team.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { MisionComponent } from './components/mision/mision.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent,
+    ProgramsComponent,
+    OpportunitiesComponent,
+    ProjectsComponent,
+    TeamComponent,
+    ContactComponent,
+    MisionComponent
   ],
   imports: [
     BrowserModule,
